5e5ea7221fd8314baa377c3f111aa73831f549fb
tonylindorock <54872194+tonylindorock@users.noreply.github.com>
2021-10-25 14:25:30 -0400

    Started

---
+++ Interactive/js/script.js
@@ -0,0 +1,23 @@
+/*****************
+
+Title of Project
+Yichen Wang
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+
+function preload() {
+
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+
+}
+function draw() {
+  background(255);
+
+}
